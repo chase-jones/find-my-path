af7cba4375f4e0b6e262e03d57b8867685676190
Chase <chase32003@gmail.com>
2020-07-03 22:57:13 -0400

    Added reduce_loc function that Sofia added.

--- Data_Processing.py
+++ Data_Processing.py
@@ -11,6 +11,12 @@ def main():
 if __name__ == '__main__':
     main()
 
+def reduce_loc(zones_dataframe):
+    zone_list = zones_dataframe['Id'].tolist()
+    colnames = []
+    for x in zone_list:
+        colnames.append(str(x))
+    return data.loc[zone_list,colnames].head()
 
 def df_item_to_id(aList, df):
     id_list = []
