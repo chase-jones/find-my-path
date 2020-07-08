6a68469c84e8fcfdf8dc35818c11db7bc1998749
satzrodt <67914646+satzrodt@users.noreply.github.com>
2020-07-08 13:34:55 -0400

    Integrated get_ordered_id_list function. Now returns item descriptions
    1) Updated functions
    2) Now returns dataframe specifying order to pick up items (by id) -- see Chase's function
    3) "User friendly" output now returns item descriptions. These item descriptions are in list form - that's the only way I could get it to work. I'm sure we could convert it to string too, but on second thought I realized a list might actually be the best if a customer has to pick up multiple items from one zone.
    4) Still returns list of ordered zones (including entrance and exit)
    5) I left the route distance code in there in case we want to fix it at a future time. However, I don't think it's necessary and I don't mind if we delete it.

--- Google-OR.py
+++ Google-OR.py
@@ -11,14 +11,21 @@ df_test = pd.read_csv('C:/Users/satzr/Desktop/Senior Design/GoogleTSPwithClasses
 # input grocerylist
 myGroceryList = ['AGAVE,DRIED (SOUTHWEST)','ACORNS,DRIED','ALLSPICE,GROUND','ALMONDS,BLANCHED']
 
-def df_itemtoid(aList,df):
+def df_itemtoid(aList,df): #used to build sol dataframe
     id_list = []
     for item in aList:
         id_list.append(df.loc[df['Description'] == item ,'Id'].values[0])
     res=pd.DataFrame(id_list,columns=['Id'])
     return res
 
-def df_idtozone(aList,df): # modified to include entrance and exit in zone list
+def df_idtozone(aList,df): # used to build sol dataframe
+    zone_list = []
+    for item in aList:
+        zone_list.append(df.loc[df['Description'] == item, 'Zone Number'].values[0])
+    result=pd.DataFrame(zone_list,columns =['Zone'])
+    return result
+
+def df_idtozone_withenterexit(aList,df): # modified to include entrance and exit in zone list
     zone_list = [-1]
     for item in aList:
         zone_list.append(df.loc[df['Description'] == item, 'Zone Number'].values[0])
@@ -26,9 +33,10 @@ def df_idtozone(aList,df): # modified to include entrance and exit in zone list
     result=pd.DataFrame(zone_list,columns =['Zone'])
     return result
 
-def df_idzone(df1,df2):
+def df_idzone(df1,df2): # builds sol dataframe with description, id, and zone
     # merge both data frames
-    frames=[df1,df2]
+    desc = pd.DataFrame(myGroceryList,columns=['Description'])
+    frames=[desc,df1,df2]
     df_f = pd.concat(frames,axis=1)
     return df_f
 
@@ -41,12 +49,35 @@ def reduce_loc(zones_dataframe, data):
     df2 = df1[df1.index.isin(zone_list)]
     return df2
 
+def get_ordered_id_list(df_list_ids_and_zones, list_ordered_zones): # creates dataframe with the order to pick up items by item id
+    output_df = pd.DataFrame(columns=['Order_num', 'Item_ID'])
+    i = 1
+    for value in list_ordered_zones:
+        # Generates a series based on the current zone. Stores in temp_series
+        temp_series = df_list_ids_and_zones.loc[df_list_ids_and_zones['Zone'] == value, 'Id']
+
+        for index, value in temp_series.items():
+            new_row = {'Order_num': i, 'Item_ID': value}
+            output_df = output_df.append(new_row, ignore_index=True)
+            i += 1
+    return output_df
+
+def get_singledesc(df_list_ids_and_zones, value): # takes in a zone, returns the items needed from that zone
+    temp_series = df_list_ids_and_zones.loc[df_list_ids_and_zones['Zone'] == value, 'Description'] #.item()
+    result = []
+    for index, value in temp_series.items():
+        result.append(value)
+    return result
+
+t1=df_itemtoid(myGroceryList,df_test)
 t2=df_idtozone(myGroceryList,df_test)
+t3=df_idtozone_withenterexit(myGroceryList,df_test)
+sol=df_idzone(t1,t2)
 
 #  initialize zone pivot data
 data = pd.read_csv('C:/Users/satzr/Desktop/Senior Design/GoogleTSPwithClasses/Zone Distanced Pivoted.csv', index_col=0)
 
-s_distmx = reduce_loc(t2, data)
+s_distmx = reduce_loc(t3, data)
 solver_distmx = s_distmx.values.tolist()
 
 def create_data_model():
@@ -61,24 +92,27 @@ def print_solution(data, manager, routing, solution):
     index = routing.Start(0)
     plan_output = 'Building route for customer list...\n'
     route_distance = 0
-    listoutput = [] # used to build list output
+    listoutput = [] # used to build list output for future program use
     while not routing.IsEnd(index):
-        listoutput.append(s_distmx.columns[index]) # appends zone number to list. to build the headers we use the pandas dataframe
+        zonenumber = s_distmx.columns[index] # to build the headers we use the pandas dataframe. zonenumber is a string (column header)
+        itemdescription = get_singledesc(sol,int(zonenumber))
+        listoutput.append(int(zonenumber)) # appends zone number to list.
         if index == 0:
             plan_output += '\n Start at store entrance.' # store entrance will always be first
         else:
-            plan_output += '\n Go to Zone %s ' %(s_distmx.columns[index]) # used to build 'user friendly' output
+            plan_output += '\n Go to Zone %s and get %s.' %(zonenumber, itemdescription) # used to build 'user friendly' output
         previous_index = index
         index = solution.Value(routing.NextVar(index))
         route_distance += routing.GetArcCostForVehicle(
             previous_index, index, 0)
     plan_output += '\n Go to checkout.' # checkout will always be last
-    listoutput.append(s_distmx.columns[index])
-    for i in range(len(listoutput)):
-        listoutput[i] = int(listoutput[i])
+    listoutput.append(int(s_distmx.columns[index]))
     plan_output += '\n Distance of the route: {}m\n'.format(route_distance) # route_distance is an integer
     print(plan_output)
     print(listoutput)
+    
+    df_orderedids = get_ordered_id_list(sol,listoutput)
+    print(df_orderedids)
 
 def main():
     """Entry point of the program."""
