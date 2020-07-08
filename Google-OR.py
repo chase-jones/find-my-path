0822126589df0e1e6e6102870e406a95c619a719
satzrodt <67914646+satzrodt@users.noreply.github.com>
2020-07-08 09:50:00 -0400

    Fixed solution printing bug, modified output
    The printed solution now provides a "customer friendly" word form and a "program friendly" list form.
    Both paths provide an ordered list of zones, but not an ordered list of items.

--- Google-OR.py
+++ Google-OR.py
@@ -1,12 +1,14 @@
+from __future__ import print_function
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
 
 import numpy as np
 import pandas as pd
 
-# nicole code - read from csv
+# initialize food data
 df_test = pd.read_csv('C:/Users/satzr/Desktop/Senior Design/GoogleTSPwithClasses/New Full SKU List.csv')
 
+# input grocerylist
 myGroceryList = ['AGAVE,DRIED (SOUTHWEST)','ACORNS,DRIED','ALLSPICE,GROUND','ALMONDS,BLANCHED']
 
 def df_itemtoid(aList,df):
@@ -58,80 +60,93 @@ solver_distmx = reduce_loc(t2)
 print(solver_distmx)
 
 ### TSP SOLVER ###
+# this version uses the sample CSV_Nicole
+s_distmx = pd.read_csv('C:/Users/satzr/Desktop/Senior Design/GoogleTSPwithClasses/CSV_Nicole.csv', index_col=0, encoding='latin-1', engine='python')
+solver_distmx = s_distmx.values.tolist()
 
 def create_data_model():
     """Stores the data for the problem."""
     data = {}
-    data['distance_matrix'] = solver_distmx # example had it as list but pandas df seems to work
-    # yapf: disable
+    data['distance_matrix'] = solver_distmx # to solve the problem we use the solver_distmx in list form
     data['num_vehicles'] = 1
-    data['starts'] = [0] # first item, index of start location, needs to be array
-    data['ends'] = [len(solver_distmx)] #last item, index of end location, needs to be array
+    data['starts'] = [0]
+    data['ends'] = [len(solver_distmx)-1]
     return data
 
-data = create_data_model()
-manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
-                                       data['num_vehicles'], data['starts'], data['ends'])
-routing = pywrapcp.RoutingModel(manager)
-
-def distance_callback(from_index, to_index):
-    """Returns the distance between the two nodes."""
-    # Convert from routing variable Index to distance matrix NodeIndex.
-    from_node = manager.IndexToNode(from_index)
-    to_node = manager.IndexToNode(to_index)
-    return data['distance_matrix'][from_node][to_node]
-
-transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-
-routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-
-search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-search_parameters.first_solution_strategy = (
-    routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-
 def print_solution(data, manager, routing, solution):
-    print('Objective: {} feet'.format(solution.ObjectiveValue()))
+    """Prints solution on console."""
     index = routing.Start(0)
-    plan_output = 'Route for customer:\n'
+    plan_output = 'Building route for customer list...\n'
     route_distance = 0
-    output = []
+    listoutput = [] # used to build list output
     while not routing.IsEnd(index):
-        output.append(solver_distmx.columns[index]) # appends zone number
-        plan_output += '\n From Zone {}, get %s -> '.format(manager.IndexToNode(index)) # this will map the indices, not zones.. printing to show that the solver works
+        listoutput.append(s_distmx.columns[index]) # appends zone number to list. to build the headers we use the pandas dataframe
+        if index == 0:
+            plan_output += '\n Start at store entrance.' # store entrance will always be first
+        else:
+            plan_output += '\n Go to Zone %s ' %(s_distmx.columns[index]) # used to build 'user friendly' output
         previous_index = index
         index = solution.Value(routing.NextVar(index))
-        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
-    plan_output += '\n Go to checkout.' # in the original code it sent us back to 0 here.
-    #plan_output += 'Zone {}\n'.format(manager.IndexToNode(index)) #editing this only affected the very last Zone 0 (last point) so the last point must not be attached to the rest of the code
+        route_distance += routing.GetArcCostForVehicle(
+            previous_index, index, 0)
+    plan_output += '\n Go to checkout.' # checkout will always be last
+    listoutput.append(s_distmx.columns[index])
+    for i in range(len(listoutput)):
+        listoutput[i] = int(listoutput[i])
+    plan_output += '\n Distance of the route: {}m\n'.format(route_distance)
     print(plan_output)
-    plan_output += 'Route distance: {}feet\n'.format(route_distance)
-    print('List form output:', output)
+    print(listoutput)
 
-solution = routing.SolveWithParameters(search_parameters)
-if solution:
-    print_solution(manager, routing, solution)
+def main():
+    """Entry point of the program."""
+    # Instantiate the data problem.
+    data = create_data_model()
 
-# # chase's code
-#     def main():
-#         df_grocery_list = pd.read_csv('C:/Users/satzr/Desktop/Senior Design/GoogleTSPwithClasses/100 Random Rows.csv')
-#         df_big_matrix = pd.read_csv('C:/Users/satzr/Desktop/Senior Design/GoogleTSPwithClasses/Distance Data Pivot.csv')
-#         df_item_descriptions = df_grocery_list.loc[:, "Description"]
-#
-#     if __name__ == '__main__':
-#         main()
-#
-#     def get_ordered_id_list(df_list_ids_and_zones, df_ordered_zones):
-#         output_df = pd.DataFrame(columns=['Order_num', 'Item_ID'])
-#         i = 1
-#         for index, value in df_ordered_zones.items():
-#             # Generates a series based on the current zone. Stores in temp_series
-#             temp_series = df_list_ids_and_zones.loc[df_list_ids_and_zones['Zone'] == value, 'Id']
-#
-#             for index, value in temp_series.items():
-#                 new_row = {'Order_num': i, 'Item_ID': value}
-#                 output_df = output_df.append(new_row, ignore_index=True)
-#                 i += 1
-#         return output_df
-#
-#     def id_desc(id, df):
-#         return df.loc[df['Id' == id, 'Description'].values[0]]
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                           data['num_vehicles'], data['starts'],
+                                           data['ends'])
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Add Distance constraint.
+    dimension_name = 'Distance'
+    routing.AddDimension(
+        transit_callback_index,
+        0,  # no slack
+        999999,  # vehicle maximum travel distance
+        True,  # start cumul to zero
+        dimension_name)
+    distance_dimension = routing.GetDimensionOrDie(dimension_name)
+    distance_dimension.SetGlobalSpanCostCoefficient(100)
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution(data, manager, routing, solution)
+
+
+if __name__ == '__main__':
+    main()
