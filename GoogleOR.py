712166a16c69cd1df0893ce595b88c4758b42eb5
satzrodt <satzrodt@gmail.com>
2020-07-11 17:02:38 -0400

    Temporarily commented out any functions that use the df_ids_zones_descriptions because we are unable to initialize/pass it to the function since we have carts of ids, not descriptions. This will allow it to work for the purposes of timing the code.

--- GoogleOR.py
+++ GoogleOR.py
@@ -5,76 +5,6 @@ import numpy as np
 import pandas as pd
 import Data_Processing as dp
 
-# # initialize food data
-# df_test = pd.read_csv('C:/Users/satzr/Desktop/Senior Design/GoogleTSPwithClasses/New Full SKU List.csv')
-#
-# # input grocerylist
-# myGroceryList = ['AGAVE,DRIED (SOUTHWEST)','ACORNS,DRIED','ALLSPICE,GROUND','ALMONDS,BLANCHED']
-
-# def df_itemtoid(aList,df): #used to build sol dataframe
-#     id_list = []
-#     for item in aList:
-#         id_list.append(df.loc[df['Description'] == item ,'Id'].values[0])
-#     res=pd.DataFrame(id_list,columns=['Id'])
-#     return res
-#
-# def df_idtozone(aList,df): # used to build sol dataframe
-#     zone_list = []
-#     for item in aList:
-#         zone_list.append(df.loc[df['Description'] == item, 'Zone Number'].values[0])
-#     result=pd.DataFrame(zone_list,columns =['Zone'])
-#     return result
-#
-# def df_idtozone_withenterexit(aList,df): # modified to include entrance and exit in zone list
-#     zone_list = [-1]
-#     for item in aList:
-#         zone_list.append(df.loc[df['Description'] == item, 'Zone Number'].values[0])
-#     zone_list.append(999999999)
-#     result=pd.DataFrame(zone_list,columns =['Zone'])
-#     return result
-#
-# def df_idzone(df1,df2): # builds sol dataframe with description, id, and zone
-#     # merge both data frames
-#     desc = pd.DataFrame(myGroceryList,columns=['Description'])
-#     frames=[desc,df1,df2]
-#     df_f = pd.concat(frames,axis=1)
-#     return df_f
-#
-# def reduce_loc(zones_dataframe, data):
-#     zone_list = zones_dataframe['Zone'].tolist()
-#     colnames = []
-#     for x in zone_list:
-#         colnames.append(str(x))
-#     df1 = data[colnames]
-#     df2 = df1[df1.index.isin(zone_list)]
-#     return df2
-#
-# def get_ordered_id_list(df_list_ids_and_zones, list_ordered_zones): # creates dataframe with the order to pick up items by item id
-#     output_df = pd.DataFrame(columns=['Order_num', 'Item_ID'])
-#     i = 1
-#     for value in list_ordered_zones:
-#         # Generates a series based on the current zone. Stores in temp_series
-#         temp_series = df_list_ids_and_zones.loc[df_list_ids_and_zones['Zone'] == value, 'Id']
-#
-#         for index, value in temp_series.items():
-#             new_row = {'Order_num': i, 'Item_ID': value}
-#             output_df = output_df.append(new_row, ignore_index=True)
-#             i += 1
-#     return output_df
-#
-# def get_singledesc(df_list_ids_and_zones, value): # takes in a zone, returns the items needed from that zone
-#     temp_series = df_list_ids_and_zones.loc[df_list_ids_and_zones['Zone'] == value, 'Description'] #.item()
-#     result = []
-#     for index, value in temp_series.items():
-#         result.append(value)
-#     return result
-#
-# t1=df_itemtoid(myGroceryList,df_test)
-# t2=df_idtozone(myGroceryList,df_test)
-# t3=df_idtozone_withenterexit(myGroceryList,df_test)
-# sol=df_idzone(t1,t2)
-
-#  initialize zone pivot data
 data = pd.read_csv('C:/Users/satzr/Desktop/Senior Design/GoogleTSPwithClasses/Zone Distanced Pivoted.csv', index_col=0)
 
 def solve_tsp(s_distmx):
@@ -94,12 +24,12 @@ def solve_tsp(s_distmx):
         listoutput = [] # used to build list output for future program use
         while not routing.IsEnd(index):
             zonenumber = s_distmx.columns[index] # to build the headers we use the pandas dataframe. zonenumber is a string (column header)
-            itemdescription = dp.get_single_desc(sol,int(zonenumber))
+            # itemdescription = dp.get_single_desc(sol,int(zonenumber))
             listoutput.append(int(zonenumber)) # appends zone number to list.
             if index == 0:
                 plan_output += '\n Start at store entrance.' # store entrance will always be first
             else:
-                plan_output += '\n Go to Zone %s and get %s.' %(zonenumber, itemdescription) # used to build 'user friendly' output
+                plan_output += '\n Go to Zone %s and get [item description].' %(zonenumber) # itemdescription, used to build 'user friendly' output
             previous_index = index
             index = solution.Value(routing.NextVar(index))
             route_distance += routing.GetArcCostForVehicle(
@@ -109,10 +39,12 @@ def solve_tsp(s_distmx):
         plan_output += '\n Distance of the route: {}m\n'.format(route_distance) # route_distance is an integer
         print(plan_output)
         print(listoutput)
+        return listoutput
 
-        df_orderedids = dp.get_ordered_id_list(sol,listoutput)
-        print(df_orderedids)
-        return df_orderedids
+        # uncomment this once we are running carts of descriptions, not ids.
+        # df_orderedids = dp.get_ordered_id_list(sol,listoutput)
+        # print(df_orderedids)
+        # return df_orderedids
 
     def main():
         """Entry point of the program."""
